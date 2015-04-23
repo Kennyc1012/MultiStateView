34fb484467fb4e81f06fb054a06c2c6ebd829de6
Kenny Campagna <kennyc1012@gmail.com>
2015-04-22 20:10:58 -0400

    Update Sample

--- sample/src/main/java/com/kennyc/sample/MainActivity.java
+++ sample/src/main/java/com/kennyc/sample/MainActivity.java
@@ -1,17 +1,22 @@
 package com.kennyc.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.kennyc.view.MultiStateView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private TestHandler mHandler = new TestHandler();
 
     private MultiStateView mMultiStateView;
 
@@ -25,6 +30,10 @@ public class MainActivity extends ActionBarActivity {
                     @Override
                     public void onClick(View v) {
                         mMultiStateView.setViewState(MultiStateView.ViewState.LOADING);
+                        Toast.makeText(getApplicationContext(), "Fetching Data", Toast.LENGTH_SHORT).show();
+                        Message msg = mHandler.obtainMessage();
+                        msg.obj = mMultiStateView;
+                        mHandler.sendMessageDelayed(msg, 3000);
                     }
                 });
 
@@ -32,10 +41,16 @@ public class MainActivity extends ActionBarActivity {
 
         String[] data = new String[100];
         for (int i = 0; i < 100; i++) {
-            data[i] = String.valueOf(i);
+            data[i] = "Row " + i;
         }
 
         list.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, data));
+    }
+
+    @Override
+    protected void onPause() {
+        mHandler.removeCallbacksAndMessages(null);
+        super.onPause();
     }
 
     @Override
@@ -65,5 +80,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private static class TestHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.obj instanceof MultiStateView) {
+                ((MultiStateView) msg.obj).setViewState(MultiStateView.ViewState.CONTENT);
+            }
+
+            super.handleMessage(msg);
+        }
     }
 }
